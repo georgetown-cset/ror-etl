0807aa17c619d4248a7bf26ff1aaa49e69405270
Jennifer Melot <jtmelot@gmail.com>
2022-11-13 09:00:45 -0500

    Update dag bucket

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -1,4 +1,4 @@
-gsutil cp ror_dag.py gs://us-east1-production2023-87afc3c5-bucket/dags/
-gsutil -m cp -r ror_scripts gs://us-east1-production2023-87afc3c5-bucket/dags/
-gsutil -m cp -r schemas/* gs://us-east1-production2023-87afc3c5-bucket/dags/schemas/ror/
+gsutil cp ror_dag.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/
+gsutil -m cp -r ror_scripts gs://us-east1-production2023-cc1-01d75926-bucket/dags/
+gsutil -m cp -r schemas/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/schemas/ror/
 gsutil -m cp -r schemas gs://airflow-data-exchange/ror/
